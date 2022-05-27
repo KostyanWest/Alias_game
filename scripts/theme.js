e73c7796172c47bafff16e03ebe26537c0ac6760
KostyanWest <63469605+KostyanWest@users.noreply.github.com>
2022-05-27 15:18:36 +0300

    Вынесение скриптов из index.html

---
+++ scripts/theme.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('lightThemeButtonElement').onclick = () => {
+        document.querySelector('.body_viewport').classList.remove('color-scheme_dark');
+        document.getElementById('lightThemeButtonElement').classList.add('hidden');
+        document.getElementById('darkThemeButtonElement').classList.remove('hidden');
+        document.getElementById('lightGithubLogoElement').classList.add('hidden');
+        document.getElementById('darkGithubLogoElement').classList.remove('hidden');
+    };
+    document.getElementById('darkThemeButtonElement').onclick = () => {
+        document.querySelector('.body_viewport').classList.add('color-scheme_dark');
+        document.getElementById('darkThemeButtonElement').classList.add('hidden');
+        document.getElementById('lightThemeButtonElement').classList.remove('hidden');
+        document.getElementById('darkGithubLogoElement').classList.add('hidden');
+        document.getElementById('lightGithubLogoElement').classList.remove('hidden');
+    };
+});
